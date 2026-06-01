7c4c72be957c4debbb211229785682316107040d
Nate Nichols <nate.nichols@utili-source.com>
2026-06-01 13:35:57 -0500

    Add iOS and Android configuration to app.config.js and update package.json scripts

--- apps/playground-rn/app.config.js
+++ apps/playground-rn/app.config.js
@@ -6,6 +6,12 @@ const baseExpo = {
 	scheme: "melon-playground",
 	orientation: "portrait",
 	newArchEnabled: true,
+	ios: {
+		bundleIdentifier: "com.nate.nichols.playgroundrn",
+	},
+	android: {
+		package: "com.nate.nichols.playgroundrn",
+	},
 	plugins: ["expo-router", "expo-sqlite", "expo-asset"],
 	experiments: {
 		typedRoutes: true,
